92f26631a011e76b066f249d0f7f8422030fc29a
ShyamPrakashPPK <shyamprakash0019@gmail.com>
2024-01-10 19:26:17 +0530

    card bg

--- src/components/Cards/TotalCard.tsx
+++ src/components/Cards/TotalCard.tsx
@@ -1,7 +1,7 @@
 const TotalCard = () => {
     return (
         <div className="h-[98%]">
-            <div className="rounded-xl bg-cover bg-center h-full p-10 bg-[url('src/assets/totlacardbg.png')] bg-blue-500">
+            <div className="rounded-xl bg-cover bg-center h-full p-10  bg-gradient-to-r from-sky-400 to-blue-500">
                 <div className="flex flex-col">
                     <div className="mt-2 font-medium text-white text-2xl">
                         Number of Paid Invoices
@@ -22,3 +22,6 @@ const TotalCard = () => {
 }
 
 export default TotalCard;
+
+
+// bg-[url('src/assets/totlacardbg.png')]     // add this to set bg image
